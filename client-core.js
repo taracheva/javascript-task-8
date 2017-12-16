310895a3b8f4d0460a17fbd0c49f5d4fad1d747d
Taracheva Ekaterina <katyataracheva@gmail.com>
2017-12-16 17:17:49 +0500

    1

--- client-core.js
+++ client-core.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.execute = execute;
-module.exports.isStar = true;
+module.exports.isStar = false;
 
 const request = require('request');
 const chalk = require('chalk');
